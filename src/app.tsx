139b5360a96b8b19883a075f4dba15edf08a7a1c
韬韫 <zhantao.zt@koubei.com>
2018-12-08 20:07:35 +0800

    feat: add async/await

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import Taro, { Component, Config } from '@tarojs/taro'
 import { Provider } from '@tarojs/mobx'
+import '@tarojs/async-await'
 import Index from './pages/index'
 
 import counterStore from './store/counter'
