fb5c03428bc3b5792889f6936e7e498cb33c4448
eightpigs <eightpigs@outlook.com>
2018-12-08 17:19:17 +0800

    补充文章、Feed 和 Collection 结构

--- src/model/post.ts
+++ src/model/post.ts
@@ -1,12 +1,42 @@
+/**
+ * 文章的标签
+ */
 export type Tag = {
   id: number,
   name: string
 }
 
+/**
+ * 文章内容
+ */
 export type Post = {
   id: number,
   title: string,
   content: string,
   date: Date,
   tagList: Tag[]
+}
+
+/**
+ * 订阅的内容
+ */
+export type Feed = {
+  id: string,
+  icon: string,
+  subscribers: number,
+  title: string,
+  topics: string[],
+  visualUrl: string,
+  wbsite: string,
+  feedId: string
+}
+
+/**
+ * 集合
+ */
+export type Collection = {
+  id: string,
+  name: string,
+  feeds: Feed[],
+  posts: Post[]
 }
