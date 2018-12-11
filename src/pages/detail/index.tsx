5512827324b8d380fd0068e9e8824e8f545c37a8
韬韫 <zhantao.zt@koubei.com>
2018-12-11 15:46:11 +0800

    feat: update detail loading

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -27,31 +27,30 @@ class Index extends Component {
   }
 
   @observable post
-  @observable msg = '加载中...'
-  @observable status: 'error' | 'loading' | 'success' = 'loading'
 
   componentWillMount () {
     const { id, index } = this.$router.params
     this.post = this.props.postsStore.collections[index].posts.find(post => post.id === id)
+    Taro.showLoading({
+      title: '加载中...'
+    }).then(res => console.log(res))
   }
 
   handleError = () => {
-    this.status = 'error'
-    this.msg = '加载失败'
+    Taro.showToast({
+      title: '加载失败',
+      icon: 'none',
+      duration: 2000
+    })
   }
 
   handleLoad = () => {
-    this.status = 'success'
+    Taro.hideLoading()
   }
 
   render () {
     return (
       <View>
-        <AtToast
-          isOpened={this.status !== 'success'}
-          text={this.msg}
-          status={this.status}
-        />
         <WebView src={this.post.originUrl} onLoad={this.handleLoad} onError={this.handleError} />
       </View>
     )
